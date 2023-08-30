18aadeb73e9ba3723c59d8e21c5daa86f0ec76cf
vk041499 <vk041499@gmail.com>
2023-08-30 08:18:15 +0530

    final

--- survey/src/Question.js
+++ survey/src/Question.js
@@ -1,149 +1,58 @@
-// const questionsAndAnswers = [
-//     {
-//       question: "Have you used our service before?",
-//       options: ["Paris", "London", "Berlin", "Rome"],
-//       answer: "Paris"
-//     },
-//     {
-//       question: "Which programming language is commonly used for web development?",
-//       options: ["Java", "Python", "JavaScript", "C++"],
-//       answer: "JavaScript"
-//     },
-//     {
-//       question: "What is the largest planet in our solar system?",
-//       options: ["Venus", "Jupiter", "Mars", "Saturn"],
-//       answer: "Jupiter"
-//     },
-//     {
-//       question: "Who is the author of the Harry Potter book series?",
-//       options: ["J.R.R. Tolkien", "J.K. Rowling", "George R.R. Martin", "Stephen King"],
-//       answer: "J.K. Rowling"
-//     },
-//     {
-//       question: "Which country is famous for the Taj Mahal?",
-//       options: ["India", "China", "Egypt", "Brazil"],
-//       answer: "India"
-//     },
-//     {
-//       question: "What does HTML stand for?",
-//       options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hypertext Transfer Protocol"],
-//       answer: "Hyper Text Markup Language"
-//     },
-//     {
-//       question: "What is the chemical symbol for water?",
-//       options: ["O", "H2O", "W", "H2"],
-//       answer: "H2O"
-//     },
-//     {
-//       question: "Which planet is known as the 'Red Planet'?",
-//       options: ["Venus", "Jupiter", "Mars", "Saturn"],
-//       answer: "Mars"
-//     },
-//     {
-//       question: "What is the main function of the CPU in a computer?",
-//       options: ["Display images on the screen", "Store data", "Perform calculations", "Connect to the internet"],
-//       answer: "Perform calculations"
-//     },
-//     {
-//       question: "What is the largest mammal on Earth?",
-//       options: ["Elephant",  "Blue Whale", "Giraffe", "Lion"],
-//       answer: "Blue Whale"
-//     }
-//   ];
 const questionsAndAnswers = [
-  {
-    question: "Do you have any experience using our services in the past?",
-    options: [
-      "Yes, frequently",
-      "Yes, on occasion",
-      "No, but I'm familiar with them",
-      "No, I've never heard of them"
-    ]
-  },
-  {
-    question: "How would you rate your overall satisfaction with the quality of our service on a scale from 1 to 10?",
-    options: [
-      "2 (Very Dissatisfied)",
-      "5 (Neutral)",
-      "8",
-      "9 (Very Satisfied)"
-    ]
-  },
-  {
-    question: "Which aspect of our service is most important to you? (Choose one)",
-    options: [
-      "Convenience",
-      "Reliability",
-      "Customer Support",
-      "Value for Money"
-    ]
-  },
-  {
-    question: "If you could change one thing about our service to make it better, what would it be?",
-    options: [
-      "Quicker service delivery",
-      "Increased product variety",
-      "Enhanced communication with customers",
-      "Other (please specify)"
-    ]
-  },
-  {
-    question: "How frequently do you engage with our services?",
-    options: [
-      "Every day",
-      "Weekly",
-      "Once a month",
-      "Rarely"
-    ]
-  },
-  {
-    question: "How probable is it that you would suggest our service to friends or family?",
-    options: [
-      "Extremely Likely",
-      "Likely",
-      "Unlikely",
-      "Extremely Unlikely"
-    ]
-  },
-  {
-    question: "What played a significant role in your decision to pick our service over alternatives?",
-    options: [
-      "Positive reputation",
-      "Competitive pricing",
-      "Unique features",
-      "Other (please specify)"
-    ]
-  },
-  {
-    question: "Have you come across any challenges while using our service?",
-    options: [
-      "Yes",
-      "No",
-      "Occasionally",
-      "Frequently"
-    ]
-  },
-  {
-    question: "If you responded 'Yes' to the prior question, please explain the problems you've faced.",
-    options: [
-      "Technical glitches",
-      "Unsatisfactory customer service",
-      "Billing discrepancies",
-      "Other (please specify)"
-    ]
-  },
-  {
-    question: "Would you be interested in participating in upcoming user testing or offering additional feedback?",
-    options: [
-      "Yes",
-      "No",
-      "Maybe",
-      "Never"
-    ]
-  }
-];
+    {
+      question: "What is your preferred mode of transportation?",
+      options: ["Car", "Public transportation", "Bicycle", "Walking"],
+      
+    },
+    {
+      question: "How often do you exercise in a week?",
+      options: ["Everyday", "3-4 times a week", "1-2 times a week", "Rarely or never"],
+      
+    },
+    {
+      question: "Which type of cuisine do you enjoy the most?",
+      options: ["Italian", "Indian", "Mexican", "American"],
+      
+    },
+    {
+      question: "How do you prefer to spend your weekends?",
+      options: ["Relaxing at home", "Hanging out with friends", "Exploring nature", "Attending cultural events"],
+      
+    },
+    {
+      question: "What's your favorite genre of music?",
+      options: ["Pop", "Rock", "Hip-hop/Rap", "Classical"],
+      
+    },
+    {
+      question: "Which social media platform do you use the most?",
+      options: ["Facebook", "Instagram", "Twitter", "Snapchat"],
+      
+    },
+    {
+      question: "What is your level of interest in politics?",
+      options: ["Very interested", "Somewhat interested", "Not very interested", "Not interested at all"],
+      
+    },
+    {
+      question: "How do you prefer to consume books?",
+      options: ["Physical copies", "E-books", "Audiobooks", "I don't read books"],
+      
+    },
+    {
+      question: "What's your favorite outdoor activity?",
+      options: ["Hiking", "Swimming", "Playing sports", "Picnicking"],
+      
+    },
+    {
+      question: "How often do you try new restaurants or food places?",
+      options: ["Weekly",  "Monthly", "Occasionally", "Rarely or never"],
+      
+    }
+  ];
 
-// Now you can use the newQuestionsAndAnswers array in your code as needed.
+
+  // Now you can use the newQuestionsAndAnswers array in your code as needed.
 
 
   export default questionsAndAnswers;
